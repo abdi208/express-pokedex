e21f02d9a8b8aded8c0109816ab00bfaf25f2d4c
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-10-14 09:46:08 -0700

    pokemon hmwrk

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const db = require('./models')
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static('static'));
+
 
 // GET / - main index of site
 app.get('/', function(req, res) {
@@ -19,6 +21,7 @@ app.get('/', function(req, res) {
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
 });
+
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
+const db = require('../models')
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then(function(pokemonfaves){
+    // console.log('Found:', pokemon.name)
+    res.render('faves', {faves: pokemonfaves})
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(function([pokemon, created]){
+    console.log(`${pokemon.name}  is ${created ? 'now in my faves':'already a fave'}`)
+    res.redirect('/pokemon')
+  })
+  
 });
 
+router.get('/:id', function(req, res) {
+  db.pokemon.findOrCreate({
+    where: {
+      id : req.params.id
+    }
+  })
+  .then(function(foundpokeid) {
+      res.render('show', {favesid: foundpokeid})
+    
+  })
+})
 module.exports = router;
